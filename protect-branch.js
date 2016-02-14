4423197644e8655d31e0207faa71fdd349f0cd45
ChrisBAshton <chrisashtonweb@gmail.com>
2016-02-14 19:30:17 +0000

    detect pushed branch

--- protect-branch.js
+++ protect-branch.js
@@ -1,14 +1,13 @@
 module.exports = function (data, process) {
 
-    // @TODO - get data.parameters.branch. If null, default to 'master'
-    //
-    // Listen for the 'push' event:
-    // https://developer.github.com/v3/activity/events/types/#pushevent
-    //
-    // If the push was a standard push (i.e. commits, not tags), check the branch.
-    // If the branch was data.parameters.branch, we need to branch off, open a pull request,
-    // switch back to data.parameters.branch, and revert the pushed commit.
+    var branchToProtect = data.parameters.branch || 'master';
 
-
-    process.succeed();
+    if ( data.payload.ref === ('refs/heads/' + branchToProtect) ) {
+        process.fail('direct push to ' + branchToProtect + ' detected!');
+        // @TODO we need to branch off, open a pull request,
+        // switch back to branchToProtect, and revert the pushed commit.
+    }
+    else {
+        process.succeed('the commit was to ' + data.payload.ref + ' and thus was not a problem');
+    }
 }
